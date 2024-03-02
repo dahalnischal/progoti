46c760726dd7dd860d7fc1700794f83430b3e981
dahalnischal <dahalnischal2057@gmail.com>
2024-03-02 18:45:44 +0545

    home page fully responsive complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var text = "PROGOTI EXCHANGE";
+  var index = 0;
+  var loadingText = document.getElementById("loadingText");
+
+  function typeWriter() {
+    if (index < text.length) {
+      loadingText.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, 150); // Adjust typing speed
+    } else {
+      // Hide loader and show site content
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("siteContent").style.display = "block";
+    }
+  }
+
+  typeWriter();
+});
+
 // SLIDER
 $(document).ready(function () {
   $(window).on("scroll", function () {
@@ -7,6 +27,87 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("small");
     }
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $(".visible-menu").addClass("small");
+    } else {
+      $(".visible-menu").removeClass("small");
+    }
+  });
+  // HAMBURGER
+  $(".menuindividual").click(function () {
+    $(".menucontents").removeClass("active");
+  });
+  $(".ham-icon").click(function () {
+    $(".menucontents").toggleClass("active");
+  });
+  $(".cross-icon").click(function () {
+    $(".menucontents").toggleClass("active");
+  });
+  // ACCORDIAN section
+  var btns = document.querySelectorAll(".menuaccordians");
+  // fn
+  function accordion() {
+    // this = the btn | icon & bg changed
+    this.classList.toggle("is-open");
+    // the acc-content
+    var content = this.nextElementSibling;
+    // IF open, close | else open
+    if (content.style.maxHeight) content.style.maxHeight = null;
+    else content.style.maxHeight = content.scrollHeight + "px";
+  }
+  // event
+  btns.forEach((el) => el.addEventListener("click", accordion));
+
+  // PARTNERS
+
+  var swiper = new Swiper(".moneySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 60,
+    // margin:20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 9000,
+      running: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      860: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1110: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
   });
   //   BANNER SWIPER
   var swiper = new Swiper(".banner-swiper", {
@@ -26,4 +127,130 @@ $(document).ready(function () {
       prevEl: ".swiper-button-prev",
     },
   });
+});
+
+// TESTIMONIAL
+var swiper = new Swiper(".tesSwiper", {
+  slidesPerView: 2,
+  spaceBetween: 60,
+  // margin:20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 9000,
+    running: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 60,
+    },
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 60,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 60,
+    },
+    // when window width is >= 640px
+    860: {
+      slidesPerView: 1,
+      spaceBetween: 60,
+    },
+    1010: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+  },
+});
+
+// PARTNERS
+
+var swiper = new Swiper(".partnersSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 60,
+  // margin:20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 9000,
+    running: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    860: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1110: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// ACCORDION
+var $titleTab = $(".click-div");
+$(".acc-wrap:first-child")
+  .find(".click-div")
+  .addClass("active")
+  .next()
+  .stop()
+  .slideDown(300);
+$(".acc-wrap:first-child").find(".inner_content").find("p").addClass("show");
+$titleTab.on("click", function (e) {
+  e.preventDefault();
+  if ($(this).hasClass("active")) {
+    $(this).removeClass("active");
+    $(this).next().stop().slideUp(500);
+    $(this).next().find("p").removeClass("show");
+  } else {
+    $(this).addClass("active");
+    $(this).next().stop().slideDown(500);
+    $(this).parent().siblings().children(".click-div").removeClass("active");
+    $(this).parent().siblings().children(".inner_content").slideUp(500);
+    $(this)
+      .parent()
+      .siblings()
+      .children(".inner_content")
+      .find("p")
+      .removeClass("show");
+    $(this).next().find("p").addClass("show");
+  }
 });
